523b23aab8a2bc7336c3b12892301ac0b8ea416a
lin10 <lin10@camel.iis.sinica.edu.tw>
2022-08-01 13:55:35 +0800

    generate video data

--- split_frame.py
+++ split_frame.py
@@ -1,21 +1,32 @@
 import cv2
 import os 
+from absl import flags
+from absl import app
 
-type = "E"
-fileName = "e09"
-cap = cv2.VideoCapture("20220526調整動作擷取/{}/{}.mp4".format(type, fileName))
-success,image = cap.read()
-i = 0
+flags.DEFINE_string('type', 'flip', 'Path to videos.')
+flags.DEFINE_string('filename', None, 'Path to videos.')
+FLAGS = flags.FLAGS
 
-os.makedirs("{}/{}/".format(type, fileName), exist_ok=False)
-
-if not success:
-    print("failed to read file: {}".format("20220526調整動作擷取/{}/{}.mp4".format(type, fileName)))
-
-while(success):
-    cv2.imwrite("{}/{}/".format(type, fileName) + str(i) + '.jpg', image)
+def main(_argv):
+    cap = cv2.VideoCapture("20220801_Jump_重新命名/{}/{}.MOV".format(FLAGS.type, FLAGS.filename))
     success,image = cap.read()
-    i+=1
+    i = 0
 
-cap.release()
-cv2.destroyAllWindows()
+    os.makedirs("20220801/{}/{}/".format(FLAGS.type, FLAGS.filename), exist_ok=False)
+
+    if not success:
+        print("failed to read file: {}".format("20220801_Jump_重新命名/{}/{}.MOV".format(FLAGS.type, FLAGS.filename)))
+
+    while(success):
+        cv2.imwrite("20220801/{}/{}/".format(FLAGS.type, FLAGS.filename) + str(i) + '.jpg', image)
+        success,image = cap.read()
+        i+=1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
